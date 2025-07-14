875f8e3e2f699af06417a19cf59382236527ffca
dr-mahmoudattia <mahmoudattia1515@gmail.com>
2025-07-14 17:20:29 +0300

    fix : aws ip

--- proxy-server.cjs
+++ proxy-server.cjs
@@ -23,7 +23,7 @@ app.get('/api/track/:barcode', async (req, res) => {
 
     console.log(`Proxying request for barcode: ${barcode}`);
 
-    const response = await fetch(`http://13.61.183.16:3000/track/${barcode}`, {
+    const response = await fetch(`http://51.21.149.2:3000/track/${barcode}`, {
       method: 'GET',
       headers: {
         'Accept': 'application/json',
